92697f84e8874f013c1d644089aaa13c4c94f9d3
Jack Whiting <hi@jackwhiting.co.uk>
2016-10-31 14:29:24 +0000

    clean up utilities

--- classes/Cleanup.php
+++ classes/Cleanup.php
@@ -47,5 +47,85 @@ class Cleanup
 		}, 3, 0);
 
 		add_filter('use_default_gallery_style', '__return_false');
+
+		add_filter('nav_menu_item_id', '__return_null');
+	}
+
+	/**
+	 * move js to the footer.
+	 * @since    1.0.0
+	 */
+	public function js_to_footer() 
+	{
+  		remove_action('wp_head', 'wp_print_scripts');
+  		remove_action('wp_head', 'wp_print_head_scripts', 9);
+  		remove_action('wp_head', 'wp_enqueue_scripts', 1);
+	}
+
+	/**
+	 * remove versions from scripts/styles.
+	 * @since    1.0.0
+	 */
+	public function remove_script_version($src) 
+	{
+  		return $src ? esc_url(remove_query_arg('ver', $src)) : false;
+	}
+
+	/**
+	 * cleanup output of stylesheet <link> tags.
+	 * @since    1.0.0
+	 */
+	public function clean_stylesheets($input) 
+	{
+		preg_match_all("!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!", $input, $matches);
+
+		if (empty($matches[2])) return $input;
+
+		// Only display media if it is meaningful
+		$media = $matches[3][0] !== '' && $matches[3][0] !== 'all' ? ' media="' . $matches[3][0] . '"' : '';
+		return '<link rel="stylesheet" href="' . $matches[2][0] . '"' . $media . '>' . "\n";
+	}
+
+	/**
+	 * cleanup output of script <script> tags.
+	 * @since    1.0.0
+	 */
+	public function clean_scripts($input)
+	{
+		$input = str_replace("type='text/javascript' ", '', $input);
+		return str_replace("'", '"', $input);
+	}
+
+	/**
+	 * cleanup output of <body> tag, make usable classes.
+	 * @since    1.0.0
+	 */
+	public function cleanup_body($classes)
+	{
+		// Add post/page slug if not present
+		if (is_single() || is_page() && !is_front_page()) {
+			if (!in_array(basename(get_permalink()), $classes)) {
+				$classes[] = basename(get_permalink());
+			}
+		}
+
+		// Remove unnecessary classes
+		$home_id_class = 'page-id-' . get_option('page_on_front');
+		$remove_classes = [
+			'page-template-default',
+			$home_id_class
+		];
+		$classes = array_diff($classes, $remove_classes);
+
+		return $classes;
+	}
+
+	/**
+	 * remove unnecessary self-closing tags.
+	 * @since    1.0.0
+	 */
+	public function remove_self_closing_tags($input) 
+	{
+  		return str_replace(' />', '>', $input);
 	}
 }
--- classes/Core.php
+++ classes/Core.php
@@ -54,12 +54,29 @@ class Core
 	private function define_public_hooks() 
 	{
 		$cookie = new Cookies($this->version);
+		$cleanup = new Cleanup($this->version);
+
+		# cookie assets		
 		$this->loader->add_action('wp_enqueue_scripts', $cookie, 'enqueue_public_scripts');
 		$this->loader->add_action('wp_enqueue_scripts', $cookie, 'enqueue_public_styles');
 
-		# clean up head
-		$cleanup = new Cleanup($this->version);
+		# clean up head/body
 		$this->loader->add_action('init', $cleanup, 'headers');
+		$this->loader->add_filter('body_class', $cleanup, 'cleanup_body');		
+
+		# clean up scripts
+		$this->loader->add_action('wp_enqueue_scripts', $cleanup, 'js_to_footer');		
+		$this->loader->add_filter('script_loader_src', $cleanup, 'remove_script_version', 15, 1);
+		$this->loader->add_filter('script_loader_tag', $cleanup, 'clean_scripts');
+
+		# clean up styles		
+		$this->loader->add_filter('style_loader_src', $cleanup, 'remove_script_version', 15, 1);
+		$this->loader->add_filter('style_loader_tag', $cleanup, 'clean_stylesheets');
+
+		# clean tags and ids
+		$this->loader->add_filter('get_avatar', $cleanup, 'remove_self_closing_tags');
+		$this->loader->add_filter('comment_id_fields', $cleanup, 'remove_self_closing_tags');
+		$this->loader->add_filter('post_thumbnail_html', $cleanup, 'remove_self_closing_tags');
 	}
 
 	/**
