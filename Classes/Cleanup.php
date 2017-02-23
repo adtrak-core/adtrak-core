dcab8201ec3b0cddef82054ecf8fe62015018c55
Jack Whiting <hi@jackwhiting.co.uk>
2017-02-23 15:07:58 +0000

    temp removing js to footer

--- Classes/Cleanup.php
+++ Classes/Cleanup.php
@@ -57,9 +57,9 @@ class Cleanup
 	 */
 	public function js_to_footer() 
 	{
-  		remove_action('wp_head', 'wp_print_scripts');
-  		remove_action('wp_head', 'wp_print_head_scripts', 9);
-  		remove_action('wp_head', 'wp_enqueue_scripts', 1);
+//  		remove_action('wp_head', 'wp_print_scripts');
+//  		remove_action('wp_head', 'wp_print_head_scripts', 9);
+//  		remove_action('wp_head', 'wp_enqueue_scripts', 1);
 	}
 
 	/**
